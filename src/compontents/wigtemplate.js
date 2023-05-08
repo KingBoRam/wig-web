5e93b54f21c33539fb6dd9f88f00236526375c99
KingRamBo <bory2321@naver.com>
2023-05-08 15:26:29 +0900

    정리

--- src/compontents/wigtemplate.js
+++ src/compontents/wigtemplate.js
@@ -1,27 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 const WigTemplateContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const WigTemplateBlock = styled.div`
-  display: flex;
-  flex-direction: column;
   width: 500px;
   height: 600px;
   background: #d0ebff;
   border-radius: 50px;
-  margin: 0 auto;
 `;
-
 function WigTemplate({ children }) {
   return (
     <WigTemplateContainer>
-      <WigTemplateBlock>{children}</WigTemplateBlock>;
+      <WigTemplateBlock>{children}</WigTemplateBlock>
     </WigTemplateContainer>
   );
 }
-
 export default WigTemplate;
